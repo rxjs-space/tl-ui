a5f9b8f9b81ec707f3b75a3cd591ee132dc522b3
Liu Tim <rxjs-space@outlook.com>
2017-01-04 19:13:28 +0800

    comments out spec for tl-clipboard.service

--- src/app/tl-ui/tl-clipboard/tl-clipboard.service.spec.ts
+++ src/app/tl-ui/tl-clipboard/tl-clipboard.service.spec.ts
@@ -8,94 +8,95 @@ import { TlClipboardService } from './tl-clipboard.service';
 // because don't know the reason for unsuccessful document.execCommand('copy') ...
 // ... after changing the nativeElement.value
 
-@Component({
-  template: `<div class="card">
-  <h4 class="card-header">tl-clipboard examples</h4>
-  <div class="card-block">
-    <p>
-      Examples of using TlClipboardService.
-    </p>
 
-    <form>
+// @Component({
+//   template: `<div class="card">
+//   <h4 class="card-header">tl-clipboard examples</h4>
+//   <div class="card-block">
+//     <p>
+//       Examples of using TlClipboardService.
+//     </p>
 
-      <div class="input-group">
+//     <form>
 
-        <input type="text" class="form-control" placeholder="type something here ..." #el>
-        <span class="input-group-btn">
-          <button class="btn btn-secondary" type="button" 
-            (click)="copy(el)"
-          >Copy</button>
-        </span>
+//       <div class="input-group">
 
-      </div>
+//         <input type="text" class="form-control" placeholder="type something here ..." #el>
+//         <span class="input-group-btn">
+//           <button class="btn btn-secondary" type="button" 
+//             (click)="copy(el)"
+//           >Copy</button>
+//         </span>
 
-      <br>
+//       </div>
 
-      <!--<textarea class="form-control" placeholder="click here and ctrl + v to paste."
-        (click)="target.value = '';" #target>-->
-      <textarea rows="8" 
-        class="form-control"
-        placeholder="click here and ctrl + v to paste."
-        (click)="target.value = '';" #target></textarea>
-    </form>
+//       <br>
 
-    <hr>
-    <div>
-      <pre><code [innerHTML]="codeExample" #code>
-      </code></pre>
-      <button class="btn btn-outline-primary" (click)="copy(code)">Copy above code.</button>
-    </div>
-  </div>
-</div>`,
-})
-export class TestHostComponent {
-  codeExample = `@Component({
-  selector: 'tl-clipboard-examples',
-  templateUrl: './tl-clipboard-examples.component.html',
-  styleUrls: ['./tl-clipboard-examples.component.scss'],
-  animations: [TlSlideInOutAnimation]
-})`;
-  constructor(private clipboard: TlClipboardService) { }
+//       <!--<textarea class="form-control" placeholder="click here and ctrl + v to paste."
+//         (click)="target.value = '';" #target>-->
+//       <textarea rows="8" 
+//         class="form-control"
+//         placeholder="click here and ctrl + v to paste."
+//         (click)="target.value = '';" #target></textarea>
+//     </form>
 
-  copy(element: any) {
-    this.clipboard.copy(element);
-  }
+//     <hr>
+//     <div>
+//       <pre><code [innerHTML]="codeExample" #code>
+//       </code></pre>
+//       <button class="btn btn-outline-primary" (click)="copy(code)">Copy above code.</button>
+//     </div>
+//   </div>
+// </div>`,
+// })
+// export class TestHostComponent {
+//   codeExample = `@Component({
+//   selector: 'tl-clipboard-examples',
+//   templateUrl: './tl-clipboard-examples.component.html',
+//   styleUrls: ['./tl-clipboard-examples.component.scss'],
+//   animations: [TlSlideInOutAnimation]
+// })`;
+//   constructor(private clipboard: TlClipboardService) { }
 
-}
+//   copy(element: any) {
+//     this.clipboard.copy(element);
+//   }
 
-
-xdescribe('TlClipboardService', () => {
-
-  let hostCmp: TestHostComponent;
-  let hostFxt: ComponentFixture<TestHostComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [TestHostComponent],
-      providers: [TlClipboardService]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    hostFxt  = TestBed.createComponent(TestHostComponent);
-    hostCmp = hostFxt.componentInstance;
-    hostFxt.detectChanges();
-  });
+// }
 
 
-  it('should ...', inject([TlClipboardService], (service: TlClipboardService) => {
-    expect(service).toBeTruthy();
-  }));
+// xdescribe('TlClipboardService', () => {
 
-  it('should copy from input element', () => {
-    let inputEl = hostFxt.debugElement.query(By.css('input'));
-    inputEl.nativeElement.value = 'xyz';
-    console.log(inputEl.nativeElement);
-    hostFxt.detectChanges();
-    let inputCopyButton = hostFxt.debugElement.query(By.css('.btn-secondary'));
-    inputCopyButton.triggerEventHandler('click', {});
-    hostFxt.detectChanges();
+//   let hostCmp: TestHostComponent;
+//   let hostFxt: ComponentFixture<TestHostComponent>;
 
-  });
-});
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [TestHostComponent],
+//       providers: [TlClipboardService]
+//     })
+//     .compileComponents();
+//   }));
+
+//   beforeEach(() => {
+//     hostFxt  = TestBed.createComponent(TestHostComponent);
+//     hostCmp = hostFxt.componentInstance;
+//     hostFxt.detectChanges();
+//   });
+
+
+//   it('should ...', inject([TlClipboardService], (service: TlClipboardService) => {
+//     expect(service).toBeTruthy();
+//   }));
+
+//   it('should copy from input element', () => {
+//     let inputEl = hostFxt.debugElement.query(By.css('input'));
+//     inputEl.nativeElement.value = 'xyz';
+//     console.log(inputEl.nativeElement);
+//     hostFxt.detectChanges();
+//     let inputCopyButton = hostFxt.debugElement.query(By.css('.btn-secondary'));
+//     inputCopyButton.triggerEventHandler('click', {});
+//     hostFxt.detectChanges();
+
+//   });
+// });
