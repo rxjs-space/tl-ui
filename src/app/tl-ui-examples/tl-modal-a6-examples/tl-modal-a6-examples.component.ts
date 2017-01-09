4d6e321a407439d00d862e65cfe338e8fb7591db
Liu Tim <rxjs-space@outlook.com>
2017-01-09 18:18:09 +0800

    wip tl-modal-a6

--- src/app/tl-ui-examples/tl-modal-a6-examples/tl-modal-a6-examples.component.ts
+++ src/app/tl-ui-examples/tl-modal-a6-examples/tl-modal-a6-examples.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { TlSlideInOutAnimation } from '../../tl-ui';
 @Component({
   selector: 'tl-modal-a6-examples',
   templateUrl: './tl-modal-a6-examples.component.html',
-  styleUrls: ['./tl-modal-a6-examples.component.scss']
+  styleUrls: ['./tl-modal-a6-examples.component.scss'],
+  animations: [TlSlideInOutAnimation]
 })
 export class TlModalA6ExamplesComponent implements OnInit {
+  modalModel = {
+    showRxx: new BehaviorSubject(false),
+  };
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
--- src/app/tl-ui/tl-modal-a6/tl-modal-a6.component.ts
+++ src/app/tl-ui/tl-modal-a6/tl-modal-a6.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, DebugElement, Input, OnInit, Renderer, ViewChild } from '@angular/core';
 @Component({
   selector: 'tl-modal-a6',
   templateUrl: './tl-modal-a6.component.html',
   styleUrls: ['./tl-modal-a6.component.scss']
 })
 export class TlModalA6Component implements OnInit {
-
-  constructor() { }
+  @Input() model: any;
+  @ViewChild('modalContainer') modalContainer: DebugElement;
+  trigger: any;
+  constructor(private renderer: Renderer) { }
 
   ngOnInit() {
+    this.model.showRxx.subscribe(v => {
+      if (v.show) {
+        this.renderer.setElementStyle(this.modalContainer.nativeElement, 'display', 'block');
+        this.renderer.invokeElementMethod(this.modalContainer.nativeElement, 'focus', []);
+        this.trigger = v.trigger;
+      } else {
+        this.renderer.setElementStyle(this.modalContainer.nativeElement, 'display', 'none');
+        if (this.trigger) {
+          this.renderer.invokeElementMethod(this.trigger, 'focus', []);
+          this.trigger = null;
+        }
+      }
+    });
   }
 
+  closeModal(info) {
+    console.log(info);
+    if (this.model.showRxx.getValue()) {
+      this.model.showRxx.next({show: false});
+    }
+  }
 }
